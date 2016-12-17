b4ad20a18f7633636347866b30dd9a2834252079
Artur Nowak <artur.nowak@live.com>
2016-12-17 10:58:34 +0100

    sync z iMac

--- fileMapTry/RandomAccessContainer/ReadMe.txt
+++ fileMapTry/RandomAccessContainer/ReadMe.txt
@@ -1,12 +1,3 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#include "stdafx.h"
-
-#include "Context.h"
-#include "rac.h"
-
-/*
-	main biblioteki
-
 	UWAGA!
 	Za³ozenie jest takie ¿e biblioteka otwiera domyœlny kontekst z folderu C:\\Menerva
 
@@ -32,25 +23,5 @@
 
 	UWAGA!!!
 	W wersji 32bity mo¿liwa jest praca z jednym kontekstem i jednym zbiorem
-*/
 
-vector<Context> CONTEXTS;
-
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-		CONTEXTS.push_back(Context(TEXT("C:\\Menerva")));
-		break;
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
-}
 
--- fileMapTry/RandomAccessContainer/rac.h
+++ fileMapTry/RandomAccessContainer/rac.h
@@ -10,8 +10,17 @@ extern vector<Context> CONTEXTS;
 __declspec(dllexport) int __cdecl vtab_CountProc(char *_NAME);
 __declspec(dllexport) char* __cdecl vtab_GetColValue(char *_NAME, int ColNr, int RowKey);
 __declspec(dllexport) void __cdecl vtab_WriteColValue(char *_NAME, char *value, int ColNr, int RowKey);
-__declspec(dllexport) char* __cdecl vtab_GetSortedValue(char *_NAME, int ColNr, int Pos, int &Key);
 
+__declspec(dllexport) void __cdecl vtab_Set4KRow(char *_NAME, char* value, int RowKey);
+__declspec(dllexport) void __cdecl vtab_Set8KRow(char *_NAME, char* value, int RowKey);
+
+__declspec(dllexport) char* __cdecl vtab_Get4KRow(char *_NAME, int RowKey);
+__declspec(dllexport) char* __cdecl vtab_Get8KRow(char *_NAME, int RowKey);
+
+
+//w przygotowaniu
+__declspec(dllexport) char* __cdecl vtab_GetSortedValue(char *_NAME, int ColNr, int Pos, int &Key);
+__declspec(dllexport) char* __cdecl vtab_GetEqualPosListProc(char *_NAME, int ColNr, char *_EqualStr);
 
 
 
